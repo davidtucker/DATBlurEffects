743b1c8ae3cb27a42a02fb07fbb476cd7cae33d0
David Tucker <david@davidtucker.net>
2014-05-07 10:34:41 -0400

    Initial Commit

---
+++ UIView+BlurImages.h
@@ -0,0 +1,20 @@
+//
+// Created by David Tucker on 1/4/14.
+// Copyright (c) 2014 David Tucker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DATBlurredActivityIndicatorView.h"
+
+typedef enum {
+    DATBlurTypeUndefined = 0,
+    DATBlurTypeLight = 1,
+    DATBlurTypeExtraLight = 2,
+    DATBlurTypeDark = 3
+} DATBlurType;
+
+@interface UIView (BlurImages)
+
+- (UIImageView *)DAT_blurredImageView:(DATBlurType)blurType;
+
+@end
---
+++ UIView+BlurImages.m
@@ -0,0 +1,49 @@
+//
+// Created by David Tucker on 1/4/14.
+// Copyright (c) 2014 David Tucker. All rights reserved.
+//
+
+#import "UIView+BlurImages.h"
+#import "UIImage+ImageEffects.h"
+#import "DATBlurredActivityIndicatorView.h"
+
+@implementation UIView (BlurImages)
+
+- (UIImageView *)DAT_blurredImageView:(DATBlurType)blurType
+{
+    // Create the image context
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, self.window.screen.scale);
+
+    // There he is! The new API method
+    [self drawViewHierarchyInRect:self.bounds afterScreenUpdates:NO];
+
+    UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIImage *blurredSnapshotImage = nil;
+    
+    switch(blurType)
+    {
+        case DATBlurTypeLight:
+            blurredSnapshotImage = [snapshotImage applyLightEffect];
+            break;
+            
+        case DATBlurTypeExtraLight:
+            blurredSnapshotImage = [snapshotImage applyExtraLightEffect];
+            break;
+            
+        case DATBlurTypeDark:
+        case DATBlurTypeUndefined:
+            blurredSnapshotImage = [snapshotImage applyDarkEffect];
+            break;
+            
+        default:
+            blurredSnapshotImage = [snapshotImage applyLightEffect];
+            break;
+    }
+
+    // Be nice and clean your mess up
+    UIGraphicsEndImageContext();
+
+    return [[UIImageView alloc] initWithImage:blurredSnapshotImage];
+}
+
+@end
